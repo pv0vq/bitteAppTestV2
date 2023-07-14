082206b55208cc0a5883f06f2036a4dbd00662b4
pv0vq <tig00566@naver.com>
2023-07-15 03:16:47 +0900

    프래그먼트 연습

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,9 +3,16 @@ package com.example.myapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
 import com.example.myapplication.databinding.ActivityMainBinding
+import com.example.myapplication.databinding.FragmentTestBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,11 +22,32 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
         setSupportActionBar(binding.toolbar)
 
+        val fragmentManager: FragmentManager = supportFragmentManager
+        val transient: FragmentTransaction = fragmentManager.beginTransaction()
+        val fragment = OneFragment()
+        // 새로운 프래그먼트를 화면에 추가
+        transient.add(R.id.fragment_content, fragment)
+        // 프래그먼트 출력
+        transient.commit()
+
 
         // 업버튼 호출 여부
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
     }
+
+    // 프래그먼트 객체
+    class OneFragment: Fragment () {
+        // 늦은 초기화
+        lateinit var binding: FragmentTestBinding
+        override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+        ): View? {
+            return inflater.inflate(R.layout.fragment_test, container, false)
+        }
+ }
 
     // 업버튼 클릭시 실행
     override fun onSupportNavigateUp(): Boolean {
